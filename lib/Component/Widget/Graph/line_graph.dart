392fdad2c738ba33635b02f577c460d80fdf6142
TjayAmit <79985747+TjayAmit@users.noreply.github.com>
2023-01-04 07:03:01 +0800

    Update line_graph.dart

--- lib/Component/Widget/Graph/line_graph.dart
+++ lib/Component/Widget/Graph/line_graph.dart
@@ -1,8 +1,10 @@
 import 'package:arduino_app/Component/Widget/Text/textwidget.dart';
+import 'package:arduino_app/Controller/main_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:get/get.dart';
 
-class LineGraph extends StatelessWidget {
+class LineGraph extends GetView<MainController> {
   const LineGraph({Key? key}) : super(key: key);
 
   @override
@@ -11,10 +13,12 @@ class LineGraph extends StatelessWidget {
       children: [
         AspectRatio(
           aspectRatio: 1.5,
-          child: SizedBox(
-              child: LineChart(
-            mainData(),
-          )),
+          child: Obx(
+            () => SizedBox(
+                child: LineChart(
+              mainData(),
+            )),
+          ),
         ),
       ],
     );
@@ -44,10 +48,10 @@ class LineGraph extends StatelessWidget {
                         title: '${meta.formattedValue} \u2109',
                         fontSize: 9,
                       ),
-                  interval: 20)),
+                  interval: 60)),
           topTitles: AxisTitles(
               axisNameWidget: const TextWidget(
-                title: 'TEMPERATURE',
+                title: 'Co\u00B2 & Tvoc Monitoring',
                 fontSize: 14,
                 fontWeight: FontWeight.w500,
               ),
@@ -58,23 +62,26 @@ class LineGraph extends StatelessWidget {
         minX: 0,
         maxX: 8,
         minY: 0,
-        maxY: 160,
+        maxY: 550,
         lineBarsData: [
           LineChartBarData(
-              spots: const [
-                FlSpot(0, 100),
-                FlSpot(2, 70),
-                FlSpot(4, 80),
-                FlSpot(6, 50),
-                FlSpot(8, 90),
-              ],
+              spots: controller.listTvocGraph,
               isCurved: true,
-              color: Colors.cyan,
+              color: controller.tvocColor.value,
               barWidth: 5,
               isStrokeCapRound: false,
               dotData: FlDotData(
                 show: true,
-              ))
+              )),
+          LineChartBarData(
+              spots: controller.listCo2Graph,
+              isCurved: true,
+              color: controller.co2Color.value,
+              barWidth: 5,
+              isStrokeCapRound: false,
+              dotData: FlDotData(
+                show: true,
+              )),
         ]);
   }
 }
