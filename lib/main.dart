d8587d48cbc50bb40b4196819fa9cc1c3ca707ff
TjayAmit <79985747+TjayAmit@users.noreply.github.com>
2023-01-04 23:33:29 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:arduino_app/Controller/main_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:arduino_app/Pages/main_view.dart';
 import 'package:get/get.dart';
 import 'package:arduino_app/Component/Binding/controllers_bindings.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:arduino_app/Controller/notification_controller.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  NotificationController controller = Get.put(NotificationController());
+  await controller.flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(NotificationController.channel);
+  Get.put(MainController());
 }
 
 class MyApp extends StatelessWidget {
